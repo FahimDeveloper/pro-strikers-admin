d9bd76d6ca98422807a94fa334f1aa5aa058fc05
FahimDeveloper <developerfahim246@gmail.com>
2024-04-23 21:54:05 +0600

    add redux api auth slice and baseQuery

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+} from "redux-persist";
+import authReducer from "./features/auth/authSlice";
+import storage from "redux-persist/lib/storage";
+import { baseApi } from "./api/baseApi";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares(),
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export default store;
