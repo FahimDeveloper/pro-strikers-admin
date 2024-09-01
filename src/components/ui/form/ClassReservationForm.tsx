dee6dca889176fb473e226b27241fc99f1b34f05
FahimDeveloper <developerfahim246@gmail.com>
2024-09-02 01:10:27 +0600

    fix some issues

--- src/components/ui/form/ClassReservationForm.tsx
+++ src/components/ui/form/ClassReservationForm.tsx
@@ -17,7 +17,7 @@ const ClassReservationForm = ({
   checkForm?: any;
   onFinish: any;
   loading: any;
-  checkData: any;
+  checkData?: any;
 }) => {
   useEffect(() => {
     if (record) {
@@ -67,109 +67,110 @@ const ClassReservationForm = ({
           </Form.Item>
         </Form>
       )}
-      {checkData._id && (
-        <Form onFinish={onFinish} form={form} layout="vertical">
-          <div className="grid grid-cols-2 gap-4">
-            <Form.Item
-              label="First Name"
-              name="first_name"
-              className="m-0"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here..." />
-            </Form.Item>
-            <Form.Item
-              label="Last Name"
-              name="last_name"
-              className="m-0"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here..." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="Email"
-              name="email"
-              rules={[{ required: true }]}
-            >
-              <Input className="m-0" placeholder="Type here.." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="Phone"
-              name="phone"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here.." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="Age"
-              name="age"
-              rules={[{ required: true }]}
-            >
-              <InputNumber
-                className="w-full"
-                placeholder="Type here.."
-                min={0}
-                max={99}
-              />
-            </Form.Item>
-            <Form.Item
-              name="sport"
-              className="m-0"
-              label="Sport"
-              rules={[{ required: true, message: "Please select Sport" }]}
-            >
-              <Input readOnly placeholder="Type here.." />
-            </Form.Item>
-
-            <Form.Item
-              className="m-0"
-              label="Street Address"
-              name="street_address"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here.." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="City"
-              name="city"
-              rules={[{ required: true, message: "Enter your city" }]}
-            >
-              <Input placeholder="Type here.." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="State/Province"
-              name="state"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here.." />
-            </Form.Item>
-            <Form.Item
-              className="m-0"
-              label="Zip/Postal Code"
-              name="zip_code"
-              rules={[{ required: true }]}
-            >
-              <Input placeholder="Type here.." />
-            </Form.Item>
-          </div>
-          <div className="mt-5 flex justify-end">
-            <Form.Item className="m-0">
-              <Button
-                loading={loading}
-                htmlType="submit"
-                className="primary-btn"
+      {checkData._id ||
+        (record && (
+          <Form onFinish={onFinish} form={form} layout="vertical">
+            <div className="grid grid-cols-2 gap-4">
+              <Form.Item
+                label="First Name"
+                name="first_name"
+                className="m-0"
+                rules={[{ required: true }]}
               >
-                {record ? "Update" : "Create"}
-              </Button>
-            </Form.Item>
-          </div>
-        </Form>
-      )}
+                <Input placeholder="Type here..." />
+              </Form.Item>
+              <Form.Item
+                label="Last Name"
+                name="last_name"
+                className="m-0"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type here..." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="Email"
+                name="email"
+                rules={[{ required: true }]}
+              >
+                <Input className="m-0" placeholder="Type here.." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="Phone"
+                name="phone"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type here.." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="Age"
+                name="age"
+                rules={[{ required: true }]}
+              >
+                <InputNumber
+                  className="w-full"
+                  placeholder="Type here.."
+                  min={0}
+                  max={99}
+                />
+              </Form.Item>
+              <Form.Item
+                name="sport"
+                className="m-0"
+                label="Sport"
+                rules={[{ required: true, message: "Please select Sport" }]}
+              >
+                <Input readOnly placeholder="Type here.." />
+              </Form.Item>
+
+              <Form.Item
+                className="m-0"
+                label="Street Address"
+                name="street_address"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type here.." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="City"
+                name="city"
+                rules={[{ required: true, message: "Enter your city" }]}
+              >
+                <Input placeholder="Type here.." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="State/Province"
+                name="state"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type here.." />
+              </Form.Item>
+              <Form.Item
+                className="m-0"
+                label="Zip/Postal Code"
+                name="zip_code"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type here.." />
+              </Form.Item>
+            </div>
+            <div className="mt-5 flex justify-end">
+              <Form.Item className="m-0">
+                <Button
+                  loading={loading}
+                  htmlType="submit"
+                  className="primary-btn"
+                >
+                  {record ? "Update" : "Create"}
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
+        ))}
     </>
   );
 };
